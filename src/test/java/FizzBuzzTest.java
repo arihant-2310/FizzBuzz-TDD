3fcc2f0d53a9988c0ef8be5c28a791a683e34dab
arihant-2310 <arihant2310@gmail.com>
2021-08-09 20:09:21 +0530

    Refactor code

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,36 +1,37 @@
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class FizzBuzzTest {
 
     @Test
-    public void fizzBuzzConverterMultipleOfBothThreeAndFive(){
+    public void fizzBuzzConverterMultipleOfBothThreeAndFive() {
         FizzBuzzConverter fizzbuzz = new FizzBuzzConverter();
         assertEquals("FizzBuzz", fizzbuzz.convertToFizzbuzz(15));
     }
 
     @Test
-    public void fizzBuzzConverterMultipleOfThree(){
+    public void fizzBuzzConverterMultipleOfThree() {
         FizzBuzzConverter fizzbuzz = new FizzBuzzConverter();
         assertEquals("Fizz", fizzbuzz.convertToFizzbuzz(3));
     }
 
     @Test
-    public void fizzBuzzConverterMultipleOfFive(){
+    public void fizzBuzzConverterMultipleOfFive() {
         FizzBuzzConverter fizzbuzz = new FizzBuzzConverter();
         assertEquals("Buzz", fizzbuzz.convertToFizzbuzz(5));
     }
 
     @Test
-    public void fizzBuzzConverterReturnsNormalNumberAsItIs(){
+    public void fizzBuzzConverterReturnsNormalNumberAsItIs() {
         FizzBuzzConverter fizzbuzz = new FizzBuzzConverter();
         assertEquals("1", fizzbuzz.convertToFizzbuzz(1));
         assertEquals("2", fizzbuzz.convertToFizzbuzz(2));
     }
 
     @Test
-    public void mainTest(){
-        for(int i = 1; i <=100; i++){
+    public void mainTest() {
+        for (int i = 1; i <= 100; i++) {
             FizzBuzzConverter fizzbuzz = new FizzBuzzConverter();
             System.out.println(fizzbuzz.convertToFizzbuzz(i));
         }
